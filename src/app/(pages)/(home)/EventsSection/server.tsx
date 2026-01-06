96da27d8e93baf616477df2a67345b7c00de782d
Janaina | Frontend <janaina@l8.digital>
2026-01-06 14:58:44 -0300

    dinamic

--- src/app/(pages)/(home)/EventsSection/server.tsx
+++ src/app/(pages)/(home)/EventsSection/server.tsx
@@ -1,16 +1,18 @@
+
+export const dynamic = "force-dynamic";
+
 import { api } from "@/app/api";
 import { EventType } from "@/types/event";
-import dynamic from "next/dynamic";
-const EventsSection = dynamic(() =>
-    import('@/app/(pages)/(home)/EventsSection/client')
+import dynamicImport from "next/dynamic";
+
+// client component carregado normalmente
+const EventsSection = dynamicImport(
+  () => import("@/app/(pages)/(home)/EventsSection/client"),
+  { ssr: true } // opcional, mas explícito
 );
 
-
 export default async function ServerCalendar() {
+  const response = (await api.events.getLimited(12)) as EventType[];
 
-    const response = await api.events.getLimited(12) as EventType[];
-
-  
-
-    return <EventsSection data={response} />;
+  return <EventsSection data={response} />;
 }
