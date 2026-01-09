6b2d0b9f8566eb316deaca2463ccd5366a8a3a75
Janaina | Frontend <janaina@l8.digital>
2026-01-09 08:36:14 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,15 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
         source: '/_next/image',
         headers: [
           {
